edc3a04986d567d2fcd9ca27ceb473f7356546e9
Daniel <idanielzor@gmail.com>
2020-11-29 00:49:33 +0200

    added retrieval and display of entries, some styling

---
+++ client/src/Entry.js
@@ -0,0 +1,32 @@
+import './css/Entry.css';
+const { Component } = require("react");
+
+class Entry extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: props.entry.description,
+            amount: props.entry.amount,
+            category: props.entry.category,
+            subCategory: props.entry.subCatergory,
+            date: props.entry.timestamp,
+        };
+    }
+
+    render() {
+        return <div className="Entry">
+            <b className="EntryDesc">{this.state.description} </b>
+            <label>Amount:</label>
+            <b>{this.state.amount} </b>
+            <label>Category:</label>
+            <b>{this.state.category} </b>
+            <label>Sub-category:</label>
+            <b>{this.state.subCategory} </b>
+            <label>Date:</label>
+            <b>{this.state.date} </b>
+        </div>;
+    }
+}
+
+export default Entry;
---
+++ client/src/NewEntry.js
@@ -0,0 +1,40 @@
+import './css/NewEntry.css';
+import axios from 'axios'
+const { Component } = require("react");
+
+class NewEntry extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    addEntry() {
+        alert("Please fill all inputs!")
+        //Save inputs to state
+        // check all inputs are there
+        //send inputs using axios
+        //updated the entries (how to send alert to main app? - maybe take parameters in constructor?)
+        // axios.post('/entry/new').then((res) => {
+        //   this.loadAllEntries(); //probably bad, just add entry
+        // });
+    }
+
+    render() {
+        return <div className="NewEntry">
+            <label className="NewEntryLabel" htmlFor="desc">Description </label>
+            <input className="NewEntryInput" id="desc" name="desc"></input><br></br>
+            <label className="NewEntryLabel" htmlFor="amount">Amount </label>
+            <input className="NewEntryInput" id="amount" name="amount"></input><br></br>
+            <label className="NewEntryLabel" htmlFor="category">Category </label>
+            <input className="NewEntryInput" id="category" name="category"></input><br></br>
+            <label className="NewEntryLabel" htmlFor="subcat">Sub-Category </label>
+            <input className="NewEntryInput" id="subcat" name="subcat"></input><br></br>
+            <label className="NewEntryLabel" htmlFor="date">Date </label>
+            <input className="NewEntryInput" id="date" name="date"></input><br></br>
+            <button onClick={this.addEntry}>Add transaction</button>
+        </div>;
+    }
+
+
+}
+
+export default NewEntry;
