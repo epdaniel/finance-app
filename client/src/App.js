b9550d7d66be782dd02326334c1997ddcbf60517
Daniel <idanielzor@gmail.com>
2021-04-09 13:51:50 +0300

    Detailed entry overhaul WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './css/App.css';
 import axios from 'axios'
-import { NewEntry } from './components/NewEntry';
+import { DetailedEntry } from './components/DetailedEntry';
 import GoogleBtn from './components/GoogleBtn';
+import { Grid } from '@material-ui/core';
 import UserProfile from './components/userProfile';
 
 
@@ -13,39 +14,41 @@ function App() {
     setShowEntryModal(prev => !prev);
   }
 
-  function loadAllEntries() {
-    let id = UserProfile.getId();
-    if (id !== -1) {
-      axios.get('/entries/all', {
-        headers: {
-          id: 1  //change to UserProfile.getId();
-        }
-      }).then((res) => {
-        //update entries
-        //this.setState({ entries: res.data });
-      });
-    }
-  }
+  // function loadAllEntries() {
+  //   let id = UserProfile.getId();
+  //   if (id !== -1) {
+  //     axios.get('/entries/all', {
+  //       headers: {
+  //         id: 1  //change to UserProfile.getId();
+  //       }
+  //     }).then((res) => {
+  //       //update entries
+  //       //this.setState({ entries: res.data });
+  //     });
+  //   }
+  // }
 
   //loadAllEntries();
 
   return (
-    <div className="App">
-      <br></br>
-      <GoogleBtn></GoogleBtn>
-      <h1>Finance App</h1>
+    <>
+      <div className="App">
+        <br></br>
+        <GoogleBtn></GoogleBtn>
+        <h1>Finance App</h1>
 
-      <button className="addEntryButton" onClick={toggleEntryModal}>Add entry</button>
-      <NewEntry showModal={showEntryModal} setShowModal={toggleEntryModal} />
+        <button className="addEntryButton" onClick={toggleEntryModal}>Add entry</button>
+        <DetailedEntry showModal={showEntryModal} setShowModal={toggleEntryModal} />
 
-      <h2>Transactions:</h2>
-      {/* {(!Array.isArray(this.state.entries) || this.state.entries.length === 0) ? <div>No Transactions yet.</div> :
+        <h2>Transactions:</h2>
+        {/* {(!Array.isArray(this.state.entries) || this.state.entries.length === 0) ? <div>No Transactions yet.</div> :
         this.state.entries.map((data, key) => {
           return <Entry key={key} entry={data}></Entry>;
         })
       } */}
 
-    </div >
+      </div >
+    </>
   );
 }
 
