58bd1ca85a354653dc225fbd6d27bf63a94cf212
Daniel <idanielzor@gmail.com>
2021-05-25 01:45:45 +0300

    Home reformatting

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react'
+import React, { useState } from "react";
 import Modal from "./Modal";
 import Header from "./Header";
 import { useAuth } from "./useAuth";
@@ -6,41 +6,34 @@ import EntryViewer from "./EntryViewer";
 import DetailedEntry from "./DetailedEntry";
 import { withStyles } from "@material-ui/core/styles";
 
-
-const styles = {
-
-  };
+const styles = {};
 
 const Home = () => {
     const auth = useAuth();
     const [showEntryModal, setShowEntryModal] = useState(false);
     const [showLogInError, setLogInError] = useState(false);
-  
+
     const toggleEntryModal = () => {
-      if (auth.loggedIn) setShowEntryModal((prev) => !prev);
-      else {
-        setLogInError(true);
-      }
+        if (auth.loggedIn) setShowEntryModal((prev) => !prev);
+        else {
+            setLogInError(true);
+        }
     };
 
     return (
         <>
-        <Header />
-        <button className="addEntryButton" onClick={toggleEntryModal}>
-          Add entry
-        </button>
-        {showLogInError && (
-          <p>
-            Please log in first!
-          </p>
-        )}
-        <Modal showModal={showEntryModal} setShowModal={toggleEntryModal}>
-          <DetailedEntry toggleModal={toggleEntryModal} />
-        </Modal>
-        <h2>Transactions:</h2>
-        <EntryViewer />
+            <Header />
+            <button className="addEntryButton" onClick={toggleEntryModal}>
+                Add entry
+            </button>
+            {showLogInError && <p>Please log in first!</p>}
+            <Modal showModal={showEntryModal} setShowModal={toggleEntryModal}>
+                <DetailedEntry toggleModal={toggleEntryModal} />
+            </Modal>
+            <h2>Transactions:</h2>
+            <EntryViewer />
         </>
-    )
-}
+    );
+};
 
 export default withStyles(styles)(Home);
